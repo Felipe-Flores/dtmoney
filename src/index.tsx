9d6d670d0710837a54e2c43debfbf76f29c8a81d
Felipe-Flores <felipe.floresceo@gmail.com>
2021-03-25 23:23:25 -0400

    criando funcções para o botão e tabela

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,39 @@ import { App } from './App';
 createServer({
 
   models: {
-    transaction: Model,
+    transactions: Model,
 
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer WebSite',
+          type: 'deposit',
+          category: 'Desenvolvedor',
+          amount: 6000,
+          createAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Humburguere',
+          type: 'withdraw',
+          category: 'Comida',
+          amount: 50,
+          createAt: new Date('2021-02-12 09:30:00'),
+        }
+      ],
+      
+    })
   },
 
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transaction')
+      return this.schema.all('transactions')
     })
 
     this.post('/transactions', (schema, request) => {
